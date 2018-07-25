7cff20ef30b2c0b58de4ee04cc8f6a60de909040
Jason Valadez <valadezj045@gmail.com>
2018-07-25 14:21:18 -0700

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import TopSpot from './topspot';
+
+//sets state for app component
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      topspots: []
+    }
+  }
+
+  handleMouseOver(event) {
+    let ls = event.target.id;
+    this.setState({
+      [ls]: 'block'
+    })
+  };
+
+  componentDidMount() {
+    axios.get('https://origin-top-spots-api.herokuapp.com/api/topspots').then(response => response.data).then(topspots => this.setState({ topspots }));
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <div className='container'>
+          <div className='jumbotron'>
+            <h1 id='title-h1'>San Diego Top Spots</h1>
+            <p id='title-p'>A list of the top 30 places to see in San Diego, California.</p>
+          </div>
+        </div>
+        <div className="output">
+        {this.state.topspots.map(topspot => (
+          <TopSpot
+            key={topspot.id}
+            name={topspot.name}
+            description={topspot.description}
+            location={topspot.location} />))}
+          </div>
+      </div>
+    );
+  };
+};
+
+export default App;
---
+++ src/topspot.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export default props => (
+    <div className='well'>
+        <h4>{props.name}</h4>
+        <p>{props.description}</p>
+        <a href={"https://maps.google.com/?q=" + props.location[0] + "," + props.location[1]}><button type="button">Map View</button></a>
+    </div>
+);
